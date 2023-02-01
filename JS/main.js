6ae3866adef92c59f0281edf7042d1e000c90d7f
BsaldanhaZ <bsaldanha1990@gmail.com>
2023-02-01 16:29:23 -0300

    teste

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,7 @@
-alert('que loucura padrinho')
+const robotron = document.querySelector("#robotron")
+
+robotron.addEventListener("clicou", seClicou)
+
+function seClicou() {
+    console.log("clicou que eu vi")
+}
